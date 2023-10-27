b1d409f725406b2f163ac871f6fedf62f8c4f38d
Maxim Van de Wynckel <maxim.van.de.wynckel@vub.be>
2023-10-27 15:39:26 +0200

    feat: scan mode for ble scanner

--- modules/capacitor-bluetooth/src/BLESourceNode.ts
+++ modules/capacitor-bluetooth/src/BLESourceNode.ts
@@ -3,6 +3,8 @@ import { BLEObject, BLEService, BLEUUID, MACAddress, RelativeRSSI } from '@openh
 import { BleClient, ScanMode, ScanResult } from '@capacitor-community/bluetooth-le';
 import { Capacitor } from '@capacitor/core';
 
+export { ScanMode };
+
 /**
  * BLE source node using @capacitor-community/bluetooth-le
  */
@@ -16,6 +18,8 @@ export class BLESourceNode extends SourceNode<DataFrame> {
         this.once('build', this._onBleInit.bind(this));
         this.once('destroy', this.stop.bind(this));
         this.options.source = this.source ?? new BLEObject();
+        this.options.scanMode = this.options.scanMode ?? ScanMode.SCAN_MODE_BALANCED;
+        this.options.processRaw = this.options.processRaw === undefined ? true : this.options.processRaw;
     }
 
     static checkPermissions(): Promise<PermissionStatus> {
@@ -75,7 +79,6 @@ export class BLESourceNode extends SourceNode<DataFrame> {
 
     start(): Promise<void> {
         return new Promise((resolve, reject) => {
-            const platform = Capacitor.getPlatform();
             this.logger('debug', 'Stopping BLE scan ...');
             BleClient.stopLEScan()
                 .then(() => {
@@ -83,58 +86,11 @@ export class BLESourceNode extends SourceNode<DataFrame> {
                     return BleClient.requestLEScan(
                         {
                             allowDuplicates: true,
-                            scanMode: ScanMode.SCAN_MODE_LOW_LATENCY,
+                            scanMode: this.options.scanMode,
                             services: this.options.uuids,
                         },
                         (result: ScanResult) => {
-                            if (this.options.debug) {
-                                this.logger('debug', 'BLE scan result', result);
-                            }
-                            try {
-                                const frame = new DataFrame();
-                                const uid =
-                                    platform === 'android'
-                                        ? MACAddress.fromString(result.device.deviceId)
-                                        : result.device.deviceId;
-                                const beacon = new BLEObject(uid as any);
-                                beacon.displayName = result.device.name;
-                                if (result.rawAdvertisement) {
-                                    beacon.parseAdvertisement(new Uint8Array(result.rawAdvertisement.buffer));
-                                }
-
-                                if (result.manufacturerData) {
-                                    Object.keys(result.manufacturerData).forEach((manufacturer) => {
-                                        const data = result.manufacturerData[manufacturer];
-                                        beacon.parseManufacturerData(
-                                            parseInt(manufacturer),
-                                            new Uint8Array(data.buffer),
-                                        );
-                                    });
-                                }
-                                if (result.serviceData !== undefined) {
-                                    Object.keys(result.serviceData).map((serviceKey) => {
-                                        const data = result.serviceData[serviceKey];
-                                        const serviceUUID = BLEUUID.fromString(serviceKey);
-                                        if (!beacon.getServiceByUUID(serviceUUID)) {
-                                            beacon.addService(new BLEService(serviceUUID, new Uint8Array(data.buffer)));
-                                        }
-                                    });
-                                }
-                                if (!beacon.txPower && result.txPower) {
-                                    beacon.txPower = result.txPower;
-                                }
-
-                                frame.addObject(beacon);
-
-                                frame.source = this.source;
-                                frame.source.relativePositions.forEach((pos) =>
-                                    frame.source.removeRelativePositions(pos.referenceObjectUID),
-                                );
-                                frame.source.addRelativePosition(new RelativeRSSI(beacon, result.rssi));
-                                this.push(frame);
-                            } catch (ex) {
-                                this.emit('error', ex);
-                            }
+                            this.process(result);
                         },
                     );
                 })
@@ -144,6 +100,50 @@ export class BLESourceNode extends SourceNode<DataFrame> {
                 })
                 .catch(reject);
         });
+    }
+
+    protected process(result: ScanResult) {
+        const platform = Capacitor.getPlatform();
+        try {
+            const frame = new DataFrame();
+            const uid = platform === 'android' ? MACAddress.fromString(result.device.deviceId) : result.device.deviceId;
+            const beacon = new BLEObject(uid as any);
+            beacon.displayName = result.device.name;
+            if (result.rawAdvertisement && this.options.processRaw) {
+                beacon.parseAdvertisement(new Uint8Array(result.rawAdvertisement.buffer));
+            } else {
+                if (result.manufacturerData) {
+                    Object.keys(result.manufacturerData).forEach((manufacturer) => {
+                        const data = result.manufacturerData[manufacturer];
+                        beacon.parseManufacturerData(parseInt(manufacturer), new Uint8Array(data.buffer));
+                    });
+                }
+                if (result.serviceData !== undefined) {
+                    Object.keys(result.serviceData).map((serviceKey) => {
+                        const data = result.serviceData[serviceKey];
+                        const serviceUUID = BLEUUID.fromString(serviceKey);
+                        if (!beacon.getServiceByUUID(serviceUUID)) {
+                            beacon.addService(new BLEService(serviceUUID, new Uint8Array(data.buffer)));
+                        }
+                    });
+                }
+            }
+
+            if (!beacon.txPower && result.txPower) {
+                beacon.txPower = result.txPower;
+            }
+
+            frame.addObject(beacon);
+
+            frame.source = this.source;
+            frame.source.relativePositions.forEach((pos) =>
+                frame.source.removeRelativePositions(pos.referenceObjectUID),
+            );
+            frame.source.addRelativePosition(new RelativeRSSI(beacon, result.rssi));
+            this.push(frame);
+        } catch (ex) {
+            this.emit('error', ex);
+        }
     }
 
     public onPull(): Promise<DataFrame> {
@@ -159,7 +159,11 @@ export interface BLESourceNodeOptions extends SensorSourceOptions {
      */
     uuids?: string[];
     /**
-     * Debugging mode
+     * Scanning mode
      */
-    debug?: boolean;
+    scanMode?: ScanMode;
+    /**
+     * Process raw advertisement data if available
+     */
+    processRaw?: boolean;
 }
